7efefa8d3454aa353b86b6c5b7f6cbff3db1aad7
Kairui Song <kasong@redhat.com>
2020-10-13 02:40:21 +0800

    90kernel-module: Lnstall less modules for strict hostonly

--- modules.d/90kernel-modules/module-setup.sh
+++ modules.d/90kernel-modules/module-setup.sh
@@ -24,19 +24,22 @@ installkernel() {
         for _mod in $(get_dev_module /dev/block/$1); do
             [[ " $_hostonly_drvs " != *$_mod* ]] && _hostonly_drvs+=" $_mod"
         done
-        [[ "$_hostonly_drve" ]] && return 0
+        [[ "$_hostonly_drvs" ]] && return 0
+        return 1
+    }
+
+    install_block_modules_strict () {
+        hostonly='' instmods $_hostonly_drvs
     }
 
     install_block_modules () {
-        hostonly='' \
-            instmods $_hostonly_drvs
-        hostonly="$(optional_hostonly)" \
-            instmods sg sr_mod sd_mod scsi_dh ata_piix
-
         instmods \
             scsi_dh_rdac scsi_dh_emc scsi_dh_alua \
             =ide nvme vmd \
-            virtio_blk
+            =drivers/usb/storage \
+            virtio_blk virtio_scsi
+
+        hostonly=$(optional_hostonly) =drivers/usb/storage
 
         dracut_instmods -o -s "${_blockfuncs}" "=drivers"
     }
@@ -48,7 +51,6 @@ installkernel() {
             ohci-hcd ohci-pci \
             uhci-hcd \
             xhci-hcd xhci-pci xhci-plat-hcd \
-            "=drivers/pinctrl" \
             ${NULL}
 
         hostonly=$(optional_hostonly) instmods \
@@ -56,15 +58,15 @@ installkernel() {
             "=drivers/tty/serial" \
             "=drivers/input/serio" \
             "=drivers/input/keyboard" \
-            "=drivers/usb/storage" \
             "=drivers/pci/host" \
             "=drivers/pci/controller" \
+            "=drivers/pinctrl" \
             ${NULL}
 
         instmods \
             yenta_socket \
             atkbd i8042 usbhid firewire-ohci pcmcia hv-vmbus \
-            virtio virtio_ring virtio_pci virtio_scsi pci_hyperv \
+            virtio virtio_ring virtio_pci pci_hyperv \
             "=drivers/pcmcia"
 
         if [[ "${DRACUT_ARCH:-$(uname -m)}" == arm* || "${DRACUT_ARCH:-$(uname -m)}" == aarch64 ]]; then
@@ -101,7 +103,13 @@ installkernel() {
         find_kernel_modules_external | instmods
 
         if ! [[ $hostonly ]] || for_each_host_dev_and_slaves_all record_block_dev_drv; then
-            install_block_modules
+            hostonly='' instmods sg sr_mod sd_mod scsi_dh ata_piix
+
+            if [[ "$hostonly_mode" == "strict" ]]; then
+                install_block_modules_strict
+            else
+                install_block_modules
+            fi
         fi
 
         # if not on hostonly mode, install all known filesystems,
@@ -123,7 +131,6 @@ installkernel() {
         [[ $arch == aarch64 ]] && arch=arm64
         instmods "=crypto" "=arch/$arch/crypto" "=drivers/crypto"
     fi
-    :
 }
 
 # called by dracut
